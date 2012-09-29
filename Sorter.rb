26166b97315c1b747b91ba913680c24083b3261e
P Aguilera <paguilera@yahoo.com>
2012-09-28 22:16:48 -0400

    Cleaning up the make_ext_list method

--- Sorter.rb
+++ Sorter.rb
@@ -6,34 +6,53 @@ class Sorter
     @start_dir = Dir.getwd
     @input_dir = File.join(@start_dir, "test-files")
     @output_dir = File.join(@start_dir, "sorted")
+    @file_list = Array.new
+    @ext_list = Array.new
+    @no_ext_list = Array.new
+    @hidden_list = Array.new
   end
-
+  
   def make_file_list
   # make a list of all the files in the input directory
-    @file_list = Array.new
-    Dir.entries(@input_dir).each do |filename|
-      @file_list.push(filename) unless filename[/\.\.?/].eql?(filename)
+    if Dir.exists?(@input_dir)
+      Dir.entries(@input_dir).each do |filename|
+        @file_list.push(filename) unless filename[/\.\.?/].eql?(filename)
+      end
     end
   end
   
   def make_ext_list
   # make a list of the unique extensions
-    @ext_list = Array.new
-    @no_ext_list = Array.new
-    @hidden_list = Array.new
     @file_list.each do |ext|
       temp = ext.split('.')
       if temp.length == 1
-        @ext_list.push('no_ext')
-        @no_ext_list.push(ext)
+        add_no_ext_file(ext)
+#        @ext_list.push('no_ext')
+#        @no_ext_list.push(ext)
       elsif ext[/^\./]
-        @ext_list.push('hidden')
-        @hidden_list.push(ext)
+        add_hidden_file(ext)
+#        @ext_list.push('hidden')
+#        @hidden_list.push(ext)
       else
-        @ext_list.push(temp[-1])
+        add_normal_file(temp[-1])
+#        @ext_list.push(temp[-1])
       end
     end
     @ext_list.uniq!
+  end
+  
+  def add_no_ext_file(ext)
+    @ext_list.push('no_ext')
+    @no_ext_list.push(ext)
+  end
+  
+  def add_hidden_file(ext)
+    @ext_list.push('hidden')
+    @hidden_list.push(ext)
+  end
+  
+  def add_normal_file(ext)
+    @ext_list.push(ext)
   end
 
   def make_output_dir
@@ -68,15 +87,19 @@ class Sorter
       end
     end
   end
+  
+  def run
+    make_file_list
+    make_ext_list
+    make_output_dir
+    make_ext_dirs
+    copy_files
+  end
 end
 
 if $0 == __FILE__
   test = Sorter.new
-  test.make_file_list
-  test.make_ext_list
-  test.make_output_dir
-  test.make_ext_dirs
-  test.copy_files
+  test.run
 end
 
 
